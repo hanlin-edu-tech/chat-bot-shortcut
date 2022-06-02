d91e1db59d26dd81ba076fff1af6ea277b4176f8
CH Hsu <zarose0000ds@gmail.com>
2022-06-02 16:01:55 +0800

    chore: modify API authorization method

--- deployGCF.sh
+++ deployGCF.sh
@@ -4,6 +4,7 @@ FUNCTION_NAME='convertToShortcut'
 RUNTIME='nodejs16'
 MEMORY='128'
 REGION='asia-east1'
+SERVICE_ACCOUNT='cloud-functions@chat-bot-shortcut.iam.gserviceaccount.com'
 
 cd ./functions
-gcloud functions deploy $FUNCTION_NAME --runtime $RUNTIME --trigger-http --allow-unauthenticated --set-secrets SHORTCUT_API_TOKEN=shortcut-api-token:latest --memory $MEMORY --region $REGION
+gcloud functions deploy $FUNCTION_NAME --runtime $RUNTIME --trigger-http --allow-unauthenticated --set-secrets SHORTCUT_API_TOKEN=shortcut-api-token:latest --service-account $SERVICE_ACCOUNT --memory $MEMORY --region $REGION
